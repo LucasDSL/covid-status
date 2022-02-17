45fbdfcf0983b983536cd5d5bf9b8f468463c93c
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2022-02-16 23:07:55 -0300

    getting the data from desired countries on disease.sh api

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
-
+import { schedule } from 'node-cron';
+import 'isomorphic-fetch';
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async runRoutine(): Promise<void> {
+    // Everyday at 10:30 PM run the routine inside the brackets
+    schedule('*/20 * * * * *', async () => {
+      const data = await this.getCovidStatus(['br', 'us', 'cn', 'ru']);
+      console.log(data);
+    });
+  }
+
+  formatListOfCountriesToURI(countries: string[]): string {
+    let listByComma = '';
+    countries.forEach((country) => {
+      listByComma += country + ',';
+    });
+    const uriContries = encodeURIComponent(listByComma);
+    return uriContries;
+  }
+
+  async getCovidStatus(countries: string[]): Promise<object[]> {
+    const countriesURI = this.formatListOfCountriesToURI(countries);
+    const response = await fetch(
+      `https://disease.sh/v3/covid-19/countries/${countriesURI}`,
+    );
+
+    return response.json();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, async () => await new AppService().runRoutine());
 }
 bootstrap();
